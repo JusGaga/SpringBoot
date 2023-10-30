00aef9554e00768b8aee31b3a86b133d5373cb88
JusGaga <gaetan3107@gmail.com>
2023-10-30 15:43:27 +0100

    first commit

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(value= "/api/v1")
+public class UserController {
+
+    @RequestMapping(value = "/1-param")
+    public String personNameWithParam(Model model, @RequestParam String name){
+        model.addAttribute("name", name);
+        return "1-param/hello";
+    }
+
+    @RequestMapping(value = "/2-param")
+    public String personNameWithParam2(Model model, @RequestParam String name, @RequestParam String firstname){
+        model.addAttribute("name", name);
+        model.addAttribute("firstname", firstname);
+        return "2-param/hello";
+    }
+}
